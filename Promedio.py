7aefbeb47a99682bfb3f8f5ec97b81da2751b6f0
Jhojanops <espinjhojan@gmail.com>
2025-03-18 20:16:49 -0500

    Add files via upload

---
+++ Promedio.py
@@ -0,0 +1,5 @@
+num1=float(input(“ingrese la primera clasificacion:”))
+num2=float(input(“ingrese la segunda clasificacion:”))
+num3=float(input(“ingrese la tercera clasificacion:”))
+promedio=nota1+nota2+nota3/3
+print(“el promedio es:”,)
